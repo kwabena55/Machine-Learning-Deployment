c2839d42cad6d8bd14ea19442eec80ffdd22f61d
unknown <Soloman@TSI-0025.tsg.local>
2020-07-12 10:54:57 -0600

    replace files

--- Deployment of ML models in Heroku using FLASK.py
+++ Deployment of ML models in Heroku using FLASK.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-data=pd.read_csv("hiring.csv")
 import numpy as np
 import pickle
 
@@ -23,3 +22,12 @@ X= data.iloc[:,0:3]
 #Pulling data for Y
 
 Y=data.iloc[:,-1]
+
+#Wrting a function to convert words into integergit
+def convert_to_int( word):
+    word_dict={"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"ten":10}
+    return word_dict[word]
+
+
+
+X["experience"]=X["experience"].apply(lambda x: convert_to_int(x) )
