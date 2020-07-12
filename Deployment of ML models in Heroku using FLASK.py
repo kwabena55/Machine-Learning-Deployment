b56240839163718cc6fcee2b5f713070a6c854c4
unknown <Soloman@TSI-0025.tsg.local>
2020-07-12 01:19:15 -0600

    uploaded

--- Deployment of ML models in Heroku using FLASK.py
+++ Deployment of ML models in Heroku using FLASK.py
@@ -1,3 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 data=pd.read_csv("hiring.csv")
+import numpy as np
+import pickle
+
+
+
+data=pd.read_csv("hiring.csv")
+
+#data preprocessing
+
+data['experience']= data["experience"].fillna(0)
+
+data["test_score"]=data["test_score"].fillna(data["test_score"].mean())
+
+
+
+#Pulling data for X
+
+X= data.iloc[:,0:3]
+
+#Pulling data for Y
+
+Y=data.iloc[:,-1]
